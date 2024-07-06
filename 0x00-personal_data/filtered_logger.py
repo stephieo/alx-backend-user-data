2b46b7fc0b6c1ac4d0c3c4e52e53601cd8e70f0b
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-07-06 22:23:26 +0100

    feat: custom log filter function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" filter personal data"""
+from typing import List
+import re
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, seperator: str) -> str:
+    """ returns a log message obfuscated"""
+    for field in fields:
+        obs_msg = re.sub(field+'=.*?'+separator,
+                         field+'='+redaction+separator, message)
+    return obs_msg
