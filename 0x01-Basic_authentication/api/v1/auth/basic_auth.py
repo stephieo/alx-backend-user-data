d346a9e0f1a835b54cca4f8faf128ba3ac420a82
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-07-10 18:33:30 +0100

    featL Base 64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,4 +6,18 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Basic Auth class"""
-    pass
+    def __init__(self):
+        """initialization"""
+        pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ returns Base64 part of Authorization header"""
+        if authorization_header is None:
+            return None
+        elif type(authorization_header) is not str:
+            return None
+        elif authorization_header[:6] != "Basic ":
+            return None
+
+        return authorization_header[6:]
