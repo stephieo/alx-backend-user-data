9eb14fbba4e2e720a97039a7ba362addb4261ca6
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-07-10 14:20:29 +0100

    feat: create auth exclusion list

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,8 +12,17 @@ class Auth():
         pass
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """doccccdocccc"""
-        return False
+        """doccccdocccc"""    
+        if path is None:
+            return True
+        if excluded_paths is None or excluded_paths == []:
+            return True
+
+        path = path.rstrip("/")
+        stripped = [i.rstrip("/") for i in excluded_paths]
+        if path in stripped:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """docccdoccc"""
