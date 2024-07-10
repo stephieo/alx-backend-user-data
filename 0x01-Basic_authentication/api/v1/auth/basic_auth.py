84bc0c2044e9e16955e8c217114d4115f8418ce9
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-07-10 18:04:23 +0100

    feat: include basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+""" Basic Authentication"""
+from flask import Flask, request
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Basic Auth class"""
+    pass
