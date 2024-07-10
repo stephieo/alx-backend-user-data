cc91323af572842820317646e95d89af56856c81
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-07-10 03:26:53 +0100

    feat: authentication class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env python3
+""" managing API authentication"""
+from flask import Flask, request
+from typing import List, TypeVar
+
+
+class Auth():
+    """class to manage API authentication"""
+
+    def __init__(self):
+        """initialization"""
+        pass
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """doccccdocccc"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """docccdoccc"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """socccdoccc"""
+        return None
+
