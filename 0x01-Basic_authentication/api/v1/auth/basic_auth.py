d0da1b00035967ff8c1a859071e084b575c1fbf2
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-07-11 03:06:21 +0100

    fix: import error

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,7 +4,7 @@ from flask import Flask, request
 from api.v1.auth.auth import Auth
 import base64
 from models.user import User
-
+from typing import TypeVar
 
 class BasicAuth(Auth):
     """Basic Auth class"""
