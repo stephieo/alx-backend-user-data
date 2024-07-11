6912d46e8728279ccc9d9c8a00603dce9811c37a
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-07-11 03:01:19 +0100

    feat: retrieve user based on credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 from flask import Flask, request
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -52,3 +53,21 @@ class BasicAuth(Auth):
 
         credentials = decoded_base64_authorization_header.split(':')
         return (credentials[0], credentials[1])
+
+    def user_object_from_credentials(self,
+                                     user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """returns user based on email and password"""
+        if user_email is None or type(user_email) is not str:
+            return None
+        if user_pwd is None or type(user_pwd) is not str:
+            return None
+
+        matched_users = User().search({'email': user_email})
+        if matched_users == []:
+            return None
+
+        for user in matched_users:
+            if user.is_valid_password(user_pwd):
+                return user
+            return None
