ab00a3fb7f3005c6572cd60b8b7774ba70addde2
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-07-12 18:06:37 +0100

    feat: session cookie

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -2,6 +2,7 @@
 """ managing API authentication"""
 from flask import Flask, request
 from typing import List, TypeVar
+import os
 
 
 class Auth():
@@ -33,3 +34,9 @@ class Auth():
     def current_user(self, request=None) -> TypeVar('User'):
         """socccdoccc"""
         return None
+
+    def session_cookie(self, request=None):
+        """ returns a cookie value from a request"""
+        if request is None:
+            return None
+        return request.cookies.get(os.getenv('SESSION_NAME'))
