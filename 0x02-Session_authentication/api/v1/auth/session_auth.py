7635e53c0779145e7cb4b5f6e2b6909db3427d20
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-07-12 16:21:24 +0100

    feat: im memory session storage

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,23 @@
 """Session Auth class """
 from flask import Flask, request
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """Session Authentication mechanism"""
-    pass
+    user_id_by_session_id = {}  # in-memory session storage
+
+    def __init__(self):
+        """initialization"""
+        pass
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a session id for a user_id"""
+        if user_id is None or type(user_id) is not str:
+            return None
+
+        session_id = uuid.uuid4()
+        user_id_by_session_id[session_id] = user_id
+
+        return session_id
