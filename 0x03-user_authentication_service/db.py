a36c6173febb763d43373c27df8a548615d3369e
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-07-19 15:32:44 +0100

    feat: add_user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,1 +1,45 @@
 #!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+# from typing import TypeVar
+
+
+# T = TypeVar('T', bound=Base)  #  for annotating all ORM objects
+
+class DB:
+    """DB class. Handler for Database
+
+       Methods:
+            add_user: Add user to User table
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """creates and saves a user to the user table in database"""
+        session = self._session
+        user = User(email=email, hashed_password=hashed_password)
+        session.add(user)
+        session.commit()
+        return user
