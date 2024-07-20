d11c766863e571eb72cb9c3088bb50a03e77813f
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-07-20 17:15:42 +0100

    feat: basic flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
-from flask import flask
+from flask import Flask, jsonify
 
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def home_page():
+    """homepage of flask app"""
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000", debug=True)
