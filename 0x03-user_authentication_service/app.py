11e2ea4825a7f895baeddddf76c8558309c7baaa
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-07-20 21:25:44 +0100

    feat: route to register new users

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env python3
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
 @app.route('/')
-def home_page():
+def home_page() -> str:
     """homepage of flask app"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """find user and register if non-existent"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except Exception:
+        return jsonify({"message": "user already registered"}), 400
+    # if user:
+    # else:
 
 
 if __name__ == "__main__":
