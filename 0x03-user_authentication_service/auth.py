2a2ec183350d43427ac8261f8834f735d109170d
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-07-20 13:17:09 +0100

    feat: hash password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+from user import User
+import bcrypt
+from db import DB
+""" authentication module"""
+
+
+def _hash_password(password: str) -> bytes:
+    """ creates a salted hash of a password string"""
+    pw_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed_pass = bcrypt.hashpw(pw_bytes, salt)
+    return hashed_pass
