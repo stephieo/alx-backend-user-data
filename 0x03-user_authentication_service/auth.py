6327622b644e11354b25152a5a25675066dd4747
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-07-20 16:51:57 +0100

    fix: db instance reference

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -11,3 +11,22 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_pass = bcrypt.hashpw(pw_bytes, salt)
     return hashed_pass
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """register users"""
+        user = self._db.find_user_by(email=email)
+
+        if user:
+            raise ValueError(f'User {email} already exists')
+        else:
+            hash_pw = _hash_password(password)
+            saved_user = self._db.add_user(email, hash_pw)
+            return saved_user
